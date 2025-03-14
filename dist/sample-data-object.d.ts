46e80b3a00a2b362f6327f65862e5817c74d9610
Jason Fox <jasonfox@bamboohr.com>
2025-03-14 17:07:05 -0600

    Add built files to .gitignore

--- dist/sample-data-object.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class SampleDataObject {
-    id: number;
-    name: string;
-    isActive: boolean;
-    createdAt: Date;
-    constructor(id: number, name: string, isActive: boolean, createdAt: Date);
-}
--- dist/sample-data-object.js
+++
@@ -1,8 +0,0 @@
-export class SampleDataObject {
-    constructor(id, name, isActive, createdAt) {
-        this.id = id;
-        this.name = name;
-        this.isActive = isActive;
-        this.createdAt = createdAt;
-    }
-}
